bc8bcfa1d9b5599361f3c6e70b0d9230086c951e
Steve Saporta <ssaporta@jouleassets.com>
2013-12-19 15:06:36 -0500

    File upload is working

--- src/main/java/com/stevesaporta/twoentities/controller/HelloController.java
+++ src/main/java/com/stevesaporta/twoentities/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.stevesaporta.twoentities.controller;
  
+import java.io.IOException;
+
+import org.omg.CORBA.portable.InputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.stevesaporta.twoentities.entity.Wrapper;
  
@@ -28,5 +33,16 @@ public class HelloController {
 	public String formSubmitted(Wrapper wrapper) {
 		return "thankyou";
 	}
- 
+
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	public String upload() {
+		return "upload";
+	}
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("myfile") MultipartFile myFile) throws IOException {
+	  System.out.println("Received file of size " + myFile.getSize() + " bytes");
+	  byte[] bytes = myFile.getBytes();
+	  return "upload";
+	}
 }
---
+++ src/main/webapp/WEB-INF/pages/upload.jsp
@@ -0,0 +1,4 @@
+<form method="post" enctype="multipart/form-data">
+  File: <input type="file" name="myfile"/><br/>
+  <button type="submit">Upload</button>
+</form>
